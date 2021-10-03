12e7722d72ee413393a1c3a6e8719af63c92f472
Anton Smith <smithenheimerr@gmail.com>
2021-10-03 11:47:32 -0400

    adding tipping

--- dd_bot.py
+++ dd_bot.py
@@ -1,15 +1,20 @@
 import os
 import sys
+import pandas as pd
+import numpy as np
 import re
 from datetime import datetime
 from dotenv import load_dotenv
 import random
-from discord.ext import commands
-import psycopg2
+# import psycopg2
+import sqlalchemy as sqldb
 import traceback
-import logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+
+import discord
+from discord.ext import commands
+from discord import Intents
+intents = Intents.default()
+intents.members = True
 
 from dice_roller import roll_dice
 
@@ -18,26 +23,25 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
+PG_SERVER = 'postgresql'
+PG_USER = os.getenv('POSTGRES_USER')
+PG_PW = os.getenv('POSTGRES_PASSWORD')
+PG_HOST = os.getenv('POSTGRES_HOSTNAME')
+PG_DB = os.getenv('DATABASE_NAME')
+
+USER_TABLE = 'dd_users'
+TIP_TABLE = 'transactions'
+
 # Logging Handler
-log_root = r'logs'
-date = datetime.today().strftime('%Y-%m-%d')
-log_format = logging.Formatter("%(asctime)s - %(name)s - %(funcName)s | %(levelname)s - %(lineno)s - %(message)s")
-if not os.path.exists(log_root):
-    os.makedirs(log_root)
-log_file = os.path.join(log_root, f'{date}_movie-bot.log')
-
-fh = logging.FileHandler(log_file, mode='a')
-fh.setLevel(logging.DEBUG)
-fh.setFormatter(log_format)
-logger.addHandler(fh)
-
-sh = logging.StreamHandler()
-sh.setLevel(logging.DEBUG)
-sh.setFormatter(log_format)
-logger.addHandler(sh)
+from loguru import logger
+log_path = r'logs'
+if not os.path.exists(log_path):
+    os.mkdirs(log_path)
+logger.add(sys.stderr, format="{time} {level} {message}", filter="my_module", level="INFO")
+logger.add(os.path.join(log_path, 'dd_bot.log'), rotation="00:00")
 
 # START BOT!
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(intents=intents, command_prefix='!')
 logger.debug('Bot initialized')
 
 @bot.event
@@ -46,13 +50,113 @@ async def on_ready():
 
 # COMMANDS
 
-@bot.command(name='add', help='Add a movie to the movie list')
-async def add(ctx, *args):
-    logger.debug('Parsing command')
+@bot.command(name='listmembers', help='List server members')
+async def list_members(ctx, *args):
+    ''' List current server members '''
+    logger.debug('Listing members')
+    logger.debug(args)
 
-@bot.command(name='roll', help='roll dice!')
+    guild = ctx.guild
+    await ctx.send(f'**Server Name: {guild.name}**')
+    user_list = f'Server Size: {len(guild.members)} members\n'
+    for member in guild.members:
+        user_list+=f' - {member.display_name} ({member}, {member.id})\n'
+
+    await ctx.send(user_list)
+    return
+    
+@bot.command(name='tip', help='Don\'t forget to tip your healer! (@user)')
+async def tip(ctx, *args):
+    ''' Tip server member, adding gold transaction to transaction history '''
+    logger.debug('Tipping user')
+    logger.debug(args)
+
+    timestamp = datetime.now()
+    source_user = ctx.message.author.id
+    nickname_lookup = {str(member.id):member.display_name for member in ctx.guild.members}
+    users = []
+    gp = 5
+    
+    tag_pattern = r'<@!(.*)>'
+    gp_pattern = r'(\d*)gp'
+
+    for arg in args:
+        tag_results = re.search(tag_pattern, arg)
+        if tag_results:
+            user_id = tag_results.group(1)
+            print(user_id)
+            users.append(user_id)
+            continue
+
+        gp_results = re.search(gp_pattern, arg)
+        if gp_results:
+            gp = gp_results.group(1)
+            gp = int(gp)
+            print(gp)
+            continue
+
+    for user_id in users:
+        raw_query = f""" insert into {TIP_TABLE} (id, source, gp, timestamp, server) """ \
+                    f""" values ('{user_id}','{source_user}',{gp},'{timestamp}','{ctx.guild.name}'); """
+        logger.debug(raw_query.strip())
+        scribe = Scribe()
+        result = scribe.query(raw_query)
+        scribe.close()
+        logger.debug(result)
+
+        tip_message = f'Tipping {nickname_lookup[user_id]} {gp} gold!'
+        logger.debug(tip_message)
+        await ctx.send(tip_message)
+    return
+   
+@bot.command(name='balance', help='List users and gold balances')
+async def balance(ctx, *args):
+    ''' List gold balances '''
+    logger.debug('Listing point balances')
+    logger.debug(args)
+    
+    tag_pattern = r'<@!(.*)>'
+    nickname_lookup = {str(m.id):m.display_name for m in ctx.guild.members}
+
+    scribe = Scribe()
+    raw_query = f""" select * from {TIP_TABLE} """ \
+                f"""where server like '{ctx.guild.name}' """
+    values = scribe.select(raw_query)
+    scribe.close()
+
+    columns = ['id', 'source', 'gp', 'timestamp', 'server']
+    df = pd.DataFrame(values, columns=columns)
+
+    if len(args) > 0:
+        users = []
+        for arg in args:
+            results = re.search(tag_pattern, arg)
+            if results:
+                user = results.group(1)
+                users.append(user)
+
+    else:
+        users = list(df['id'].unique())
+
+    balance_response = '** Current Gold Balance **\n'
+    for user in users:
+        try:
+            total = int(df['gp'][df['id'] == user].sum())
+            display_name = nickname_lookup[user]
+            balance_response += f'{display_name.capitalize()} - {total}gp\n'
+            logger.debug(f'{user}:{total}')
+        except:
+            err = f'Could not resolve user {user}'
+            logger.debug(err)
+            await ctx.send(err)
+
+    await ctx.send(balance_response)
+    return
+
+@bot.command(name='roll', help='roll dice! (eg. 2d6+2)')
 async def roll(ctx, *args):
     logger.debug('Parsing command')
+    logger.debug(args)
 
     input_str = ''.join(args)
     roll_total, roll_list, static_list = roll_dice(input_str)
@@ -113,7 +217,7 @@ async def on_message(message):
         f'{author_name} is an amazing human / genius',
         'Title of your sextape',
         f'{author_name}, with all due respect, I am gonna completely ignore everything you just said.',
-        f'{author_name}. Good to see you. But if you’re here, who’s guarding Hades?'
+        f'{author_name}. Good to see you. But if you’re here, who’s guarding Hades?',
     ]
 
     # Call patterns & responses!
@@ -125,7 +229,7 @@ async def on_message(message):
         (['inquisition','inquisitor','inquisitive','inquiry','inquire'], 'No one expects the Spanish Inquisition!'),
         (['treasure'], 'Maybe the real treasure was the friends we made along the way'),
         (['pivot'],'https://media.giphy.com/media/3nfqWYzKrDHEI/giphy.gif'),
-        (['boom','baby'],'https://media.giphy.com/media/11SkMd003FMgW4/giphy.gif')
+        (['boom','baby'],'https://media.giphy.com/media/11SkMd003FMgW4/giphy.gif'),
     ]
 
     # Check for easter eggs
@@ -166,6 +270,45 @@ def parse_username(author_obj):
     user = user.capitalize()
 
     return user
+    
+class Scribe:
+    def __init__(self):
+        self.engine_pattern = '{}://{}:{}@{}/{}'
+        self.engine_name = self.engine_pattern.format(PG_SERVER,
+                                                PG_USER, 
+                                                PG_PW, 
+                                                PG_HOST, 
+                                                PG_DB)
+        self.connect()
+
+    def connect(self):
+        self.engine = sqldb.create_engine(self.engine_name)
+        self.connection = self.engine.connect()
+
+    def close(self):
+        self.connection.close()
+        self.engine.dispose()
+
+    def query(self, query):
+        logger.debug('Executing query')
+        result = None
+        try:
+            result = self.connection.execute(query)
+        except Exception as e:
+            logger.error(f'Error - {e}')
+            logger.error(f'Traceback - {traceback.format_exc()}')
+        logger.debug('Success')
+
+    def select(self, query):
+        logger.debug('Executing query')
+        result = None
+        try:
+            result = self.connection.execute(query).fetchall()
+        except Exception as e:
+            logger.error(f'Error - {e}')
+            logger.error(f'Traceback - {traceback.format_exc()}')
+        logger.debug('Success')
+        return result
 
 # MAIN
 
--- sql_utils.py
+++ sql_utils.py
@@ -5,152 +5,40 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-engine_pattern = '{}://{}:{}@{}/{}'
 
 server = 'postgresql'
 user = os.getenv('POSTGRES_USER')
 pw = os.getenv('POSTGRES_PASSWORD')
 host = os.getenv('POSTGRES_HOSTNAME')
 database = os.getenv('DATABASE_NAME')
-movie_table = os.getenv('MOVIE_TABLE_NAME')
 
-engine_name = engine_pattern.format(server,
-                                    user, 
-                                    pw, 
-                                    host, 
-                                    database)
+users = 'dd_users'
+transactions = 'transactions'
 
-def add_movie(key, title, submitter, votes=1):
-    """
-    adds record to specified table with user-specified key.
-    :param conn:
-    :param table:
-    :param key:
-    :param title:
-    :param submitter:
-    :param votes:
-    :return:
-    """
+class Scribe:
+    def __init__(self):
+        self.engine_pattern = '{}://{}:{}@{}/{}'
+        print(self.engine_pattern)
+        self.engine_name = self.engine_pattern.format(server,
+                                                user, 
+                                                pw, 
+                                                host, 
+                                                database)
+        self.connect()
 
-    engine = sqldb.create_engine(engine_name)
-    with engine.connect() as connection:
+    def connect(self):
+        self.engine = sqldb.create_engine(self.engine_name)
+        self.connection = self.engine.connect()
+
+    def close(self):
+        self.connection.close()
+        self.engine.dispose()
+
+    def query(self, query):
+        result = None
         try:
-            timestamp = datetime.now().replace(microsecond=0).isoformat().replace(':','-')
-            raw_query = "INSERT INTO {} (id, title, votes, submitter, voters, timestamp) VALUES ('{}','{}',{},'{}','{}','{}');"
-            ins_query = raw_query.format(movie_table, key, title, votes, submitter, f'{submitter},',timestamp)
-            connection.execute(ins_query)
-
-        except Exception as error:
-            print("Error while inserting record", error)
-
-        finally: 
-            connection.close()
-            engine.dispose()
-    return
-
-
-def get_movie_list(col=['*']):
-    """
-    selects records from the postgres connection and returns as an array
-    :param conn: postgres connection
-    :param table: table to select from
-    :param where: free-form where statement string tacked on end of select ex. select * from table (where x = y)
-    :return: array of select statement results
-    """
-
-    if type(col) == list:
-        col = ','.join(col)
-    elif not type(col) == str:
-        return 
-
-    engine = sqldb.create_engine(engine_name)
-    with engine.connect() as connection:
-        try:
-            sel_query = f'select {col} from {movie_table}'
-            resultproxy = connection.execute(sel_query)
-            movie_collection = [{column: value for column, value in rowproxy.items()} for rowproxy in resultproxy]
-
-        except Exception as error:
-            print("Error while selecting records", error)
-            
-        finally: 
-            connection.close()
-            engine.dispose()
-
-    return movie_collection
-
-
-def updaterecord(table, field, condition, value):
-    """
-    updates records with specified conditions.
-    :param conn: postgres connection
-    :param table: table to update
-    :param field: field to update
-    :param condition: (string) conditions, probably keyfield = key
-    :param value: value to update
-    :return: none
-    """
+            result = self.connection.execute(query)
+        except Exception as e:
+            print(f'Error - {e}')
+        return result
     
-    engine = sqldb.create_engine(engine_name)
-    with engine.connect() as connection:
-        try:
-            update_query = 'UPDATE {} SET {} = {} where {}'.format(table, field, value, condition)
-            connection.execute(update_query)
-
-        except Exception as error:
-            print("Error while updating records", error)
-
-        finally:
-            connection.close()
-            engine.dispose()
-
-    return
-    
-def update_movie(title, field, value):
-    """
-    updates records with specified conditions.
-    :param conn: postgres connection
-    :param table: table to update
-    :param field: field to update
-    :param condition: (string) conditions, probably keyfield = key
-    :param value: value to update
-    :return: none
-    """
-    
-    engine = sqldb.create_engine(engine_name)
-    with engine.connect() as connection:
-        try:
-            update_query = f"UPDATE {movie_table} SET {field} = {value} where title like '{title}'"
-            connection.execute(update_query)
-
-        except Exception as error:
-            print("Error while updating records", error)
-
-        finally:
-            connection.close()
-            engine.dispose()
-
-    return
-
-def remove_movie(title):
-    """
-    deletes records with specified condition.
-    :param conn: postgres connection
-    :param table: table to update
-    :param condition: (string) conditions "where"
-    :return: none
-    """
-    engine = sqldb.create_engine(engine_name)
-    with engine.connect() as connection:
-        try:
-            delete_query = "DELETE FROM {} where title like '{}'".format(movie_table, title)
-            connection.execute(delete_query)
-
-        except Exception as error:
-            print("Error while updating records", error)
-            
-        finally:
-            connection.close()
-            engine.dispose()
-
-    return
